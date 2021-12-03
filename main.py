4acc830fa7525ffab9183434354afb2426e73f8c
Azizbek Omonkeldiev <00012121@wiut.uz>
2021-12-03 08:29:35 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# myFinalMarks = {'CSF': 75, 'FunPro': 80, 'WT': 75}
+#
+# def calculateTotal(finalMarks):
+# 	total = 0
+#
+# 	for key in finalMarks:
+# 		total = total+finalMarks[key]
+# 	return total
+#
+# def calculateAverage(finalMarks):
+# 	average = calculateTotal(finalMarks) / len(finalMarks)
+# 	return average
+#
+# print(calculateAverage(myFinalMarks))
+#
+
+
+class Student:
+	def printHello(self):
+		print("")
+
+if __name__=="__main__":
+	student1=Student()
+	student1.printHello()
+	student2=Student()
+	student2.printHello()
